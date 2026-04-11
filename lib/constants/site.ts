b2e65dde8b6178f22cd3a3b9126f11472ebe74eb
Alex <server@Alexs-Mac-mini.local>
2026-04-11 12:49:20 +0300

    hide track record from nav

--- lib/constants/site.ts
+++ lib/constants/site.ts
@@ -27,7 +27,6 @@ export const marketingNav = [
   { href: "/#preturi", label: "Prețuri" },
   { href: "/#testimoniale", label: "Testimoniale" },
   { href: "/#faq", label: "FAQ" },
-  { href: "/track-record", label: "Track Record" },
   { href: "/blog", label: "Blog" }
 ];
 
